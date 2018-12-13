463a8f9cabb514892d7c979b663c6949f8672d23
Shaeen <shaeenkevinsingh@gmail.com>
2018-12-13 09:07:19 +0200

    destroying pins on contact

---
+++ Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculator : MonoBehaviour {
+
+	float score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pins = GameObject.FindGameObjectsWithTag("Pin");
+	}
+
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+			score += 1;
+		};
+
+		Debug.Log(score);
+    }
+}
